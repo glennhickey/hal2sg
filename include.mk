f1075220d0564fdb71fd069f5c04b48296df027e
Glenn Hickey <glenn.hickey@gmail.com>
2015-04-11 13:35:25 -0400

    oops add include.mk to git

---
+++ include.mk
@@ -0,0 +1,42 @@
+binPath=${rootPath}
+libPath=${rootPath}
+
+#Modify this variable to set the location of sonLib
+sonLibRootPath=${rootPath}/../sonLib
+sonLibPath=${sonLibRootPath}/lib
+
+cactusRootPath=${rootPath}/../cactus
+cactusPath=${cactusRootPath}/lib
+
+halRootPath=${rootPath}/../hal
+halPath=${halRootPath}/lib
+
+include  ${sonLibRootPath}/include.mk
+
+cflags += -I ${sonLibPath} -I ${cactusPath} -I ${halPath} ${tokyoCabinetIncl} ${kyotoTycoonIncl}
+cppflags += -I ${sonLibPath} -I ${cactusPath} -I ${halPath} ${tokyoCabinetIncl} ${kyotoTycoonIncl} -D__STDC_LIMIT_MACROS -Wno-deprecated
+basicLibs = ${halPath}/halLiftover.a ${halPath}/halLib.a ${sonLibPath}/sonLib.a ${sonLibPath}/cuTest.a 
+basicLibsDependencies = ${basicLibs}
+
+# hdf5 compilation is done through its wrappers.
+# we can speficy our own (sonlib) compilers with these variables:
+HDF5_CXX = ${cpp}
+HDF5_CXXLINKER = ${cpp}
+HDF5_CC = ${cxx}
+HDF5_CCLINKER = ${cxx} 
+cpp = h5c++ ${h5prefix}
+cxx = h5cc ${h5prefix}
+
+# add compiler flag and kent paths if udc is enabled
+# relies on KENTSRC containing path to top level kent/ dir
+# and MACHTYPE being specified
+ifdef ENABLE_UDC
+#  Find samtabix as in kent/src/inc/common.mk:
+	ifeq (${SAMTABIXDIR},)
+		SAMTABIXDIR = /hive/data/outside/samtabix/${MACHTYPE}
+	endif
+
+	basicLibs += ${KENTSRC}/src/lib/${MACHTYPE}/jkweb.a  ${SAMTABIXDIR}/libsamtabix.a -lssl -lcrypto
+endif
+
+
